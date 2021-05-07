0cf4ae220722e2a0e9a58013e3b7070208237850
ptakkamore4 <76100936+ptakkamore4@users.noreply.github.com>
2021-05-07 18:46:13 +0530

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongodb_uri = 'mongodb+srv://ptakkamore4:mongodb@124@cluster0.ece8f.mongodb.net/Tracker?retryWrites=true&w=majority'
+const mongodb_uri = 'mongodb+srv://palak123:palak123@cluster0.ece8f.mongodb.net/Tracker?retryWrites=true&w=majority'
 
 mongoose.connect(mongodb_uri, {
     useNewUrlParser:true,
